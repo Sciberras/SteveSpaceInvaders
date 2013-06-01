fabe4d5e1ba8b1fe44486a41b07f2268c298520d
Steve <steve.sciberras@mcast.edu.mt>
2013-06-01 21:25:11 +0200

    BUG4 Fixed: Instantiate written wrong
    In AlienController.js on line 18 Instantiate was written wrong
    FIX: Instantiate written well

--- Game Assets and Project Files/Assets/Play/Scripts/AlienController.js
+++ Game Assets and Project Files/Assets/Play/Scripts/AlienController.js
@@ -15,7 +15,7 @@ function Start () {
 //causes the alien to shoot a laser.  
 function shootlaser()
 {
-	Instantiat(alienlaser,transform.position,transform.rotation);
+	Instantiate(alienlaser,transform.position,transform.rotation);
 }
 
 function Update () {
